a1e3757f6f26f6df352a92850c783425ff0d87dc
Jules0897 <125454129+Jules0897@users.noreply.github.com>
2023-06-02 17:25:02 +0200

    Delete pages/oldProjects/school/gradeOne/homework directory

--- pages/oldProjects/school/gradeOne/homework/Hoofdstuk6/voorbeeld1.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Wanneer moet de cookie verlopen? Over 14 dagen!
-    $expires = time() + 3600 * 24 * 14;
-
-    //Plaats de cookie
-    setcookie("userCountCookie", 0, $expires, "/");
-?>
--- pages/oldProjects/school/gradeOne/homework/includes/variabelen.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //Maakt een variabele aan voor de naam, klas en datum
-    $naam = "Jules Verbruggen";
-    $klas = "IO1J4";
-    $datum = date("d-m-y");
-?>
