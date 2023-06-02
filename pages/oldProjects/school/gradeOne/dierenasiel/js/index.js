6e08fd636fcfbcb173f55ef6a228a38c90d54825
Jules0897 <125454129+Jules0897@users.noreply.github.com>
2023-06-02 17:16:11 +0200

    Add files via upload

---
+++ pages/oldProjects/school/gradeOne/dierenasiel/js/index.js
@@ -0,0 +1,10 @@
+//zorgt dat de navigatie balk geopent kan worden door op het open icoontje te drukken
+function openNav() 
+{
+  document.getElementById("mySidenav").style.width = "250px";
+}
+//zorgt ervoor dat het kruisje op de navigatiebalk werkt
+function closeNav() 
+{
+  document.getElementById("mySidenav").style.width = "0";
+}
---
+++ pages/oldProjects/school/gradeOne/dierenasiel/js/index_home.js
@@ -0,0 +1,2 @@
+var name = prompt("Welkom, wat is uw naam?");
+document.getElementById("naam").innerHTML= name;
